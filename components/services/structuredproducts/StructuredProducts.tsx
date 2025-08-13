dd519befbdca5f4901371f651f1a53cbc88a9189
Bhavana-kerelly <bhavanareddykerelly@gmail.com>
2025-08-13 15:26:13 +0530

    Update StructuredProducts.tsx

--- components/services/structuredproducts/StructuredProducts.tsx
+++ components/services/structuredproducts/StructuredProducts.tsx
@@ -188,7 +188,7 @@ export default function MortgageLoans() {
 
     
     <section>
-        <div className="bg-black text-white px-6 py-16">
+        <div className="bg-[#132b4a] text-white px-6 py-16">
       <h2 className="text-3xl font-bold mb-4">
         STRUCTURED PRODUCTS
       </h2>
